c7cc69ed088d15bbfb15190bc59cf1357894846c
Alexandre Garbé <109791591+ludecieldev@users.noreply.github.com>
2023-12-06 20:57:34 +0100

    Delete example_files directory

--- example_files/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for map_file in maps/*; do
-    my_file=$(basename "$map_file")
-    ../.././setting_up "$map_file" > "test.txt"
-    diff -q "test.txt" "solved/$my_file"
-done
-rm test.txt
